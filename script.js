883ae8af86746ac4f4c77ad813e5cd9a93cebb20
Stemel93 <mauro.migliorino93@gmail.com>
2022-04-08 16:33:25 +0200

    Versione 1.0 con formattazione codice

--- script.js
+++ script.js
@@ -17,61 +17,49 @@ const mieImg = [
   "shock",
 ];
 
-let modal = $('#modal')
+let modal = $("#modal");
 modal.hide();
 
 $(document).ready(function () {
-
   $.fx.interval = 300;
-   
-  $(".progress").animate( {width: "100%" }, { 
-      duration: 7000,    
-      step: function(now, fx){ 
-      if(fx.prop == 'width') {
-                var countup = Math.round( (now / 100) * 100) + '%';
-      $(".countup").html(countup); }    
-      },            
-    
-      
-    start: function() { $(this).before("<div class='load'><p>Caricamento...</p></div>"); },  
-    
-    /* complete: function() { $('#avanti').slideDown('slow'); }, */ 
-    complete: function() { $(this).after("<div class='avanti'><button type='button' id='avanti'>Start!</button></div>"); 
-  
-    $('#avanti').on('click', () =>{
-      
-      
-      $('.progressBar').css({display: 'none'});
-      $('.circle').css({display: 'none'});
-      $('#modalStart').fadeIn('slow');
-      $('#modalStart').css({display: 'flex'});
-      $('.container').fadeIn('slow');
-      
-      
-      
 
+  $(".progress").animate(
+    { width: "100%" },
+    {
+      duration: 7000,
+      step: function (now, fx) {
+        if (fx.prop == "width") {
+          var countup = Math.round((now / 100) * 100) + "%";
+          $(".countup").html(countup);
+        }
+      },
 
-    });
+      start: function () {
+        $(this).before("<div class='load'><p>Caricamento...</p></div>");
+      },
 
-    $('#avanti').slideDown('slow');
-  },
-  
+      /* complete: function() { $('#avanti').slideDown('slow'); }, */
+      complete: function () {
+        $(this).after(
+          "<div class='avanti'><button type='button' id='avanti'>Start!</button></div>"
+        );
 
+        $("#avanti").on("click", () => {
+          $(".progressBar").css({ display: "none" });
+          $(".circle").css({ display: "none" });
+          $("#modalStart").fadeIn("slow");
+          $("#modalStart").css({ display: "flex" });
+          $(".container").fadeIn("slow");
+        });
 
-   
-    done: function() { $("div.load").html("<p>Pronto a giocare!</p>"); }
-           
-      });    
-      
-      
-     
+        $("#avanti").slideDown("slow");
+      },
 
-
-
-
-
-
-
+      done: function () {
+        $("div.load").html("<p>Pronto a giocare!</p>");
+      },
+    }
+  );
 
   shuffle(mieImg);
 
@@ -107,73 +95,65 @@ $(document).ready(function () {
   var punteggio = 0;
   var esatto = 100;
   var sbagliato = 10;
-  let timer = 0
-  let punteggioMosse = esatto - mosse
-  let punteggioTempo = esatto - timer
-  let punteggioFinal = punteggio + punteggioMosse + punteggioTempo
+  let timer = 0;
+  let punteggioMosse = esatto - mosse;
+  let punteggioTempo = esatto - timer;
+  let punteggioFinal = punteggio + punteggioMosse + punteggioTempo;
   var corsa = false;
 
-
   function startTimer() {
-    $('#tempo').html(timer)
+    $("#tempo").html(timer);
     timer++;
-
   }
 
   function startTimerCorsa() {
-    $('#tempo').html(timer)
+    $("#tempo").html(timer);
     timer--;
-    if(corsa === true && timer <= 0) {
-      $('#tempo').html('0')
-      $(images).css('pointer-events', 'none')
-      $('#modalLost').fadeIn('slow')
-      $('#modalLost').css({display: 'flex'})
+    if (corsa === true && timer <= 0) {
+      $("#tempo").html("0");
+      $(images).css("pointer-events", "none");
+      $("#modalLost").fadeIn("slow");
+      $("#modalLost").css({ display: "flex" });
       $(".punteggio").html(punteggio);
-        $('.puntiMosse').html(punteggioMosse);
-        $('.puntiTempo').html(punteggioTempo);
-        $('.punteggioFinale').html(punteggioFinal);
+      $(".puntiMosse").html(punteggioMosse);
+      $(".puntiTempo").html(punteggioTempo);
+      $(".punteggioFinale").html(punteggioFinal);
 
-      return clearInterval(clockCorsa)
-      
+      return clearInterval(clockCorsa);
     }
-
   }
 
-  $('#normalCorsa').click(function () {
-    corsa = true
-    timer = 31
-    $('#tempo').html('30')
-    $('#modalStart').slideUp('slow');
-  })
+  $("#normalCorsa").click(function () {
+    corsa = true;
+    timer = 31;
+    $("#tempo").html("30");
+    $("#modalStart").slideUp("slow");
+  });
 
-  $('#normal').click(function () {
-    corsa = false
-    timer = 0
-    $('#modalStart').slideUp('slow');
-  })
-  
+  $("#normal").click(function () {
+    corsa = false;
+    timer = 0;
+    $("#modalStart").slideUp("slow");
+  });
 
   $(images).click(function () {
-    
-
     if (corsa === false && timer <= 0) {
-      timer = 1
-      clock = setInterval(startTimer, 1000)
+      timer = 1;
+      clock = setInterval(startTimer, 1000);
     } else if (corsa === true && timer >= 31) {
-      timer = 30
-      clockCorsa = setInterval(startTimerCorsa, 1000)
+      timer = 30;
+      clockCorsa = setInterval(startTimerCorsa, 1000);
     }
-    
-    
+
     $("#clicks").html(counter);
     $("#mosse").html(mosse);
     $("#punteggio").html(punteggio);
-    $('#punteggioFinale').html(punteggioFinal);
-    $('#puntiMosse').html(punteggioMosse);
-    $('#puntiTempo').html(punteggioTempo);
+    $("#punteggioFinale").html(punteggioFinal);
+    $("#puntiMosse").html(punteggioMosse);
+    $("#puntiTempo").html(punteggioTempo);
     punteggioMosse = esatto - mosse;
     punteggioTempo = esatto - timer;
-    punteggioFinal = punteggio + punteggioMosse + punteggioTempo
+    punteggioFinal = punteggio + punteggioMosse + punteggioTempo;
     $(this).addClass("girata");
     $(this).removeClass("images");
     counter++;
@@ -181,14 +161,12 @@ $(document).ready(function () {
 
     if (match.length == 2) {
       if (match[0] == match[1]) {
-        rightCouples.push(match[0], match[1])
+        rightCouples.push(match[0], match[1]);
         console.log(rightCouples);
         if (rightCouples.length == 16) {
-
-          modal.show()
-          clearInterval(clockCorsa)
-          return clearInterval(clock)
-
+          modal.show();
+          clearInterval(clockCorsa);
+          return clearInterval(clock);
         }
         match = [];
         let cartaGiusta = $(".girata");
@@ -197,12 +175,10 @@ $(document).ready(function () {
         mosse++;
         punteggio = punteggio + esatto;
 
-
         $("#punteggio").html(punteggio);
-        $('#punteggioFinale').html(punteggioFinal);
-        $('#puntiMosse').html(punteggioMosse);
-        $('#puntiTempo').html(punteggioTempo);
-
+        $("#punteggioFinale").html(punteggioFinal);
+        $("#puntiMosse").html(punteggioMosse);
+        $("#puntiTempo").html(punteggioTempo);
       } else {
         match = [];
         setTimeout(function () {
@@ -213,8 +189,6 @@ $(document).ready(function () {
 
           punteggio = punteggio - sbagliato;
           $("#punteggio").html(punteggio);
-
-
         }, 500);
       }
     }
@@ -222,7 +196,7 @@ $(document).ready(function () {
   $("#Restart").click(function () {
     location.reload();
   });
-  $('.replay').click(function () {
-    location.reload()
-  })
+  $(".replay").click(function () {
+    location.reload();
+  });
 });
